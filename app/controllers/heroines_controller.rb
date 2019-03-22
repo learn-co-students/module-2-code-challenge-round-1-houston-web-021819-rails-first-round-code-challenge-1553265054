0159dcabdb18cfaf08fe1155db5cd146726fd336
JingChen <jingchenjc2019@gmail.com>
2019-03-22 10:49:33 -0500

    Done.Bonus search function not yet finished.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,57 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  #search function.not working now.
+  def search_power
+    Heroine.all.where('name LIKE ?', "%#{self.power.name}%")
+    redirect_to search_heroines_path(params[:q])
+  end
+
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @errors = flash[:errors] || {}
+    @heroine = Heroine.new(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params[:heroine])
+    if @heroine.valid?
+       @heroine.save
+       redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.messages
+      render :new
+    end
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])    
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine = @heroine.assign_attributes(herione_params[:heroine])
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroines_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @heroine = Heroine.find(params[:id])    
+    @heroine.destroy
+    redirect_to heroines_path
+  end
+
+  private
+  def heroine_params
+    params.permit(heroine:[:name, :super_name, :power_id])
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,48 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new(params[:id])
+  end
+
+  def create
+    @power = Power.new(power_params[:power])
+    if @power.valid?
+       @power.save
+       redirect_to @power
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @power = Power.find(params[:id])    
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power = @power.assign_attributes(power_params[:power])
+    if @power.valid?
+      @power.save
+      redirect_to powers_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @power = Power.find(params[:id])    
+    @power.destroy
+    redirect_to powers_path
+  end
+
+  private
+  def power_params
+    params.permit(power:[:name, :description])
+  end
 end
