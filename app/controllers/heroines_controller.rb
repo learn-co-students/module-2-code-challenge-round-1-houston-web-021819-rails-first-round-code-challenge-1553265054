5e297bb6dc67a0c4d1e3e0848a96f698ba25b956
Thomas_Hansen <ctrhansen@yahoo.com>
2019-03-22 10:52:56 -0500

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,45 @@
 class HeroinesController < ApplicationController
+  
+
+  #create
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+  end
+
+  #read
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  #update
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def update 
+    @heroine = Heroine.find(params[:id])
+    @heroine = Heroine.update(heroine_params)
+    redirect_to '/heroines'
+  end
+
+  #destroy
+  def destroy
+    @heroine = Heroine.find(params[:id])
+    @heroine = Herione.destroy
+    redirect_to '/heriones'
+  end
+
+
+  #strong_params
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name)
   end
 end
