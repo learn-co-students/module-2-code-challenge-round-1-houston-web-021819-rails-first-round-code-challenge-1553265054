dbc4f0315da7caf97906cee199f1f3439a796929
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-22 10:49:10 -0500

    done

--- db/migrate/20170725185425_create_heroines.rb
+++ db/migrate/20170725185425_create_heroines.rb
@@ -2,6 +2,7 @@ class CreateHeroines < ActiveRecord::Migration[5.0]
   def change
     create_table :heroines do |t|
       t.string :name
+      t.integer :power_id
       t.string :super_name
 
       t.timestamps
