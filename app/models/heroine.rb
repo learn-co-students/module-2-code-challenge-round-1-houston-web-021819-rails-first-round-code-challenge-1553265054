1ceff72a05296e5a5088bbdced5e4521ffefc376
github username <github email address>
2019-03-22 10:48:53 -0500

    done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,19 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+
+    validates :super_name, uniqueness: true
+
+    def self.search(search)
+        if search
+            power = Power.find_by(name:search)
+            if power
+               self.where(power_id: power.id)
+            else
+                Heroine.all
+            end
+        end
+    else
+         Heroine.all
+    end
 end
+  
